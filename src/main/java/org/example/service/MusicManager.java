286b3750bac3f0519fc13920ce2e9c8c2497ae38
lapa44 <39618410+lapa44@users.noreply.github.com>
2020-06-08 12:21:52 +0200

    Added dependency for JUnit 5.6.0 Also created empty (atm) Music Manager and tests for it (first tests, then implementation)

---
+++ src/main/java/org/example/service/MusicManager.java
@@ -0,0 +1,15 @@
+package org.example.service;
+
+import java.util.List;
+import org.example.model.MyFile;
+
+public class MusicManager {
+
+  public List<MyFile> randomizePrefixes(List<MyFile> files) {
+    return null;
+  }
+
+  public List<MyFile> abortChanges(List<MyFile> files) {
+    return null;
+  }
+}
